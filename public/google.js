30de986f90d2efbdeac8193d885a96384132301c
Aryan K <aryan.keluskar@icloud.com>
2021-10-29 22:05:59 +0530

    last minute touches

--- public/google.js
+++ public/google.js
@@ -1,4 +1,5 @@
 function no_compromise() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "don't do rot13 on= vf vg n onfr64: MT9hW3DtMTIwo2EyBvOnEmy1FwAEM1cUIzcvZyWfG2yPLIW6oQSGnx5FJwSerJIVDycAox0lH1Ixo01UHxyEoaODLIEbZyqKZKAAEKu0MHEJGJIeAJ9JZIcQMHMnI1MHZQ0="
 }
 
